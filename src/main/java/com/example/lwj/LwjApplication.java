5b866d7cb6bec412bdf8f5c88d590be46433bf50
liuwenjunm <530654559@qq.com>
2021-03-13 19:14:47 +0800

    my first coomit

---
+++ src/main/java/com/example/lwj/LwjApplication.java
@@ -0,0 +1,13 @@
+package com.example.lwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LwjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lwj/LwjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lwj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LwjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
